725ac23a3a4539918b9d4a83fa668584e70eacc6
rishee321 <rishabhkamde52@gmail.com>
2026-07-13 17:33:09 +0530

    array

---
+++ array/array.js
@@ -0,0 +1,11 @@
+let num = [87,4,5,67,6,3,3]
+// console.log(num[3]);//indexing of array
+// console.log(num.length); //length of array
+num[2] = 100; //updating the value of array
+console.log(num); //printing the updated value
+
+
+
+
+
+
